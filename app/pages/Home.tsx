96c1f25774b9b8024a2641865a79dd01eb8abe62
Mike <grabbou@gmail.com>
2023-03-15 09:14:05 +0400

    feat: add copy to clipboard and indicator

--- app/pages/Home.tsx
+++ app/pages/Home.tsx
@@ -1,9 +1,10 @@
 import Web3Auth, { LOGIN_PROVIDER, OPENLOGIN_NETWORK } from '@web3auth/react-native-sdk'
+import * as Clipboard from 'expo-clipboard'
 import Constants, { AppOwnership } from 'expo-constants'
 import * as Linking from 'expo-linking'
 import * as WebBrowser from 'expo-web-browser'
 import { useAtom, useAtomValue } from 'jotai'
-import { Suspense } from 'react'
+import { Suspense, useEffect, useState } from 'react'
 import { ActivityIndicator, Text, TouchableOpacity, View } from 'react-native'
 
 import { userStateAtom } from '../data/auth'
@@ -41,7 +42,11 @@ export default function Home() {
   }
   return (
     <View>
-      {user && <Suspense fallback={<ActivityIndicator />}><WalletAddress /></Suspense>}
+      {user && (
+        <Suspense fallback={<ActivityIndicator />}>
+          <WalletAddress />
+        </Suspense>
+      )}
       {user ? (
         <TouchableOpacity onPress={logout}>
           <Text>Logout</Text>
@@ -57,7 +62,25 @@ export default function Home() {
 
 const WalletAddress = () => {
   const address = useAtomValue(ethWalletAddress)
+
+  /**
+   * Little utility to show a small "Copied!" indicator after copying the address
+   * for a few seconds (configured to 5s)
+   */
+  const [copied, setCopied] = useState(false)
+  const copyToClipboard = async () => {
+    await Clipboard.setStringAsync(address)
+    setCopied(true)
+  }
+  useEffect(() => {
+    const timeout = copied && setTimeout(() => setCopied(false), 5000)
+    return () => clearTimeout(timeout)
+  })
+
   return (
-    <Text>Wallet address: {address}</Text>
+    <>
+      <Text onPress={copyToClipboard}>Wallet address: {address}</Text>
+      {copied && <Text>Copied!</Text>}
+    </>
   )
 }
